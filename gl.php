bcadbfa3b1cdce11cd0434af651d341343521176
GHOSTCHEATSS <hassanrafiq269@gmail.com>
2024-06-29 09:26:30 -0700

    Delete gl.php

--- gl.php
+++
@@ -1,1 +0,0 @@
-https://cdn.discordapp.com/attachments/1240348240788979796/1255133151542513819/GLOBAL_protected.dll?ex=667c0556&is=667ab3d6&hm=fe90f58b1d1de7e9b8c52fce03f85c7cee4b1d73be83177b538e995b2a2feb85&
